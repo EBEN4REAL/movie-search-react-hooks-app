525ebe7c76e35940b0c2d21a5988d5f9a2e8f7a2
Ebezer Igbinoba <igbinobaebenezer@gmail.com>
2022-10-28 02:13:55 +0100

    .

--- app.js
+++ app.js
@@ -6,8 +6,4 @@ const port = process.env.PORT || 3000 // Heroku will need the PORT environment v
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/', (req, res) => {
-//    res.sendFile(express.static(path.join(__dirname, 'build')));
-// });
-
 app.listen(port, () => console.log(`App is live on port ${port}!`))
